d45061c5dcf3678dd6a9f1f6baa7e6773da74ac2
Julien <jvansteenkiste2@gmail.com>
2021-07-27 16:22:04 +0200

    initial commit

---
+++ api/app.py
@@ -0,0 +1,85 @@
+import sys
+
+sys.path.insert(0, "./api/")
+from pathlib import Path
+
+import pickle
+import uvicorn
+
+from fastapi import Depends, FastAPI, HTTPException, Security, status
+from fastapi.security import OAuth2PasswordRequestForm
+from datetime import timedelta
+from classes import Token, User
+from utils import authenticate_user, create_access_token, get_current_active_user
+from database import fake_users_db
+from config import ACCESS_TOKEN_EXPIRE_MINUTES
+
+# tracing.py
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+
+from opentelemetry import trace
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+
+root = Path(".")
+model_path = root / "models" / "model.bin"
+
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "api_test"})
+    )
+)
+
+jaeger_exporter = JaegerExporter(
+    agent_host_name="localhost",
+    agent_port=6831,
+)
+
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(jaeger_exporter)
+)
+
+tracer = trace.get_tracer(__name__)
+
+# with tracer.start_as_current_span("foo"):
+
+app = FastAPI()
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username, "scopes": form_data.scopes},
+        expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer", "printformdata": form_data.scopes}
+
+
+@app.get("/")
+def main():
+    return "hello"
+
+
+@app.get("/{input}")
+def predict(input: str, current_user: User = Security(get_current_active_user, scopes=["get_predict"])):
+    tfidf, model = pickle.load(open(model_path, 'rb'))
+    predictions = model.predict(tfidf.transform([input]))
+    label = predictions[0]
+    return {'text': input, 'label': label}
+
+
+FastAPIInstrumentor.instrument_app(app)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8081)
